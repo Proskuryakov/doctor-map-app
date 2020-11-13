e045b44c5b69ffa130ba724856e51f0a7e99d700
Egor Proskuryakov <proskur.egor@yandex.ru>
2020-11-13 16:01:46 +0300

    Add rout to new page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainPage} from './routed/pages/main/main.page';
+import {MapPage} from './routed/pages/map/map.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPage
+  },
+  {
+    path: 'map',
+    component: MapPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPage } from './routed/pages/main/main.page';
+import { MainComponent } from './routed/components/main/main.component';
+import { MapComponent } from './routed/components/map/map.component';
+import { MapPage } from './routed/pages/map/map.page';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPage,
+    MainComponent,
+    MapComponent,
+    MapPage,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    LeafletModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
