c0d18616493a0e012641a3440d2d6d9b8632d138
Egor Proskuryakov <proskur.egor@yandex.ru>
2020-12-08 23:47:53 +0300

    Add create delete update and changeIllness methods

--- src/app/features/sick/services/sick-api.service.ts
+++ src/app/features/sick/services/sick-api.service.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {SickModel} from '../models/sick.model';
+import {SickModel, Address, FullName} from '../models/sick.model';
 import {IllnessModel} from '../../illness/models/illness.model';
-import {SickModule} from '../sick.module';
 import {environment} from '../../../../environments/environment';
 
 @Injectable({
@@ -14,10 +13,63 @@ export class SickApiService {
   constructor(private readonly http: HttpClient) {
   }
 
+  createSick(
+    fullName: FullName,
+    region: string,
+    city: string,
+    street: string,
+    house: string,
+    illnesses: Array<IllnessModel>
+  ): Observable<SickModel> {
+
+    let address: Address = {id: null, region: region, city: city, street: street, house: house, lat: null, lon: null};
+    let sick: SickModel = {id: null, fullName: fullName, address: address, illnesses: illnesses};
+
+    return this.http.post<SickModel>(`${environment.api}/sick/`, sick);
+  }
+
+  deleteSick(sickModel: SickModel): Observable<void> {
+    return this.http.delete<void>(`${environment.api}/sick/${sickModel.id}`);
+  }
+
+  updateSick(sickModel: SickModel): Observable<SickModel> {
+    return this.http.put<SickModel>(`${environment.api}/sick/`, {
+      id: sickModel.id,
+      fullName: sickModel.fullName,
+      address: sickModel.address
+    });
+  }
+
+  changeIllnessDependency(sickId: number, addList: IllnessModel[], deleteList: IllnessModel[]): Observable<void> {
+    let addString = this.getIllnessesString(addList);
+    let deleteString = this.getIllnessesString(deleteList);
+    let params = new HttpParams();
+    if (addString) {
+      params = params.append('add-illness', addString);
+    }
+    if (deleteString) {
+      params = params.append('delete-illness', deleteString);
+    }
+
+    return this.http.put<void>(`${environment.api}/sick/${sickId}`, {}, {params: params});
+
+  }
+
+  getAllSickWithIllness(): Observable<Array<SickModel>> {
+    return this.getSickByParameters(
+      undefined,
+      false,
+      true,
+      undefined,
+      undefined,
+      undefined,
+      undefined);
+  }
+
   getSickByParameters(
     illnessArray: Array<IllnessModel>,
     isIllnessTogether: boolean,
-    concatIllness:boolean,
+    concatIllness: boolean,
     region: string | undefined,
     city: string | undefined,
     street: string | undefined,
